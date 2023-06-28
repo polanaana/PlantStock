b03181df06367fcaaa39291f72a3c23c77fe73f1
polana <polanskaanna.x@gmail.com>
2023-06-28 18:27:28 +0200

    Implement users

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,12 @@
 
 from django.urls import path, include
 
+from . import views
+
 app_name = 'users'
 urlpatterns = [
     #Adding default urls
     path('', include('django.contrib.auth.urls')),
+    #Register
+    path('register/', views.register, name='register'),
 ]
